f0a67f64a599577a3bb0e7f4ba0c631cbf4d6bce
code-hoihoi <naoya2014@gmail.com>
2020-05-06 00:25:26 +0900

    :sparkles:58.Testing dictionary equivalence

--- section3/blog/tests/unit/post_test.py
+++ section3/blog/tests/unit/post_test.py
@@ -10,3 +10,9 @@ class PostTest(unittest.TestCase):
 
         self.assertEqual('Test', p.title)
         self.assertEqual('Test Content', p.content)
+
+    def test_json(self):
+        p = Post('Test', 'Test Content')
+        expected = {'title': 'Test', 'content': 'Test Content'}
+
+        self.assertDictEqual(expected, p.json())
