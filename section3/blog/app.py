88c1fdb594eec92f9925d2b6c2c00c91ff65b953
code-hoihoi <naoya2014@gmail.com>
2020-05-12 22:41:50 +0900

    :sparkles:64. Taking our patching further

--- section3/blog/app.py
+++ section3/blog/app.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python
 # coding: utf-8
+from blog import Blog
+from post import Post
 
 MENU_PROMPT = 'Enter "c" to create a blog, "l" to list blogs, "r" to read one, "p" to create a post, or "q" to quit: '
-blogs = dict()  # the reason not using {} is to avoid the confusion with set (set is also written as {}).
+blogs = dict()  # the reason not using {} is to avoid the confusion with set (An empty set is also initialized with {}).
 
 
 def menu():
@@ -14,7 +16,16 @@ def menu():
     """
     print_blogs()
     selection = input(MENU_PROMPT)
-
+    while selection != 'q':
+        if selection == 'c':
+            ask_create_blog()
+        elif selection == 'l':
+            print_blogs()
+        elif selection == 'r':
+            ask_read_blog()
+        elif selection == 'p':
+            ask_create_post()
+        selection = input(MENU_PROMPT)
 
 def print_blogs():
     """
@@ -22,3 +33,31 @@ def print_blogs():
     """
     for key, blog in blogs.items():
         print(f"- {blog}")
+
+
+def ask_create_blog():
+    title = input('Enter the blog\'s title: ')
+    author = input('Enter the author\'s name: ')
+    b = Blog(title, author)
+    blogs[title] = b
+
+
+def ask_read_blog():
+    title = input('Enter the blog\'s title you wanna read: ')
+    print_posts(blogs[title])
+
+
+def print_posts(blog):
+    for post in blog.posts:
+        print_one_post(post)
+
+
+def print_one_post(post):
+    print(f"--- {post.title} --- \n\n {post.content} \n\n")
+
+
+def ask_create_post():
+    title = input('Enter the post\'s title: ')
+    content = input('Enter the post\'s content: ')
+    p = Post(title, content)
+
--- section3/blog/tests/system/app_test.py
+++ section3/blog/tests/system/app_test.py
@@ -6,13 +6,14 @@ from mock import MagicMock
 
 import app
 from blog import Blog
+from post import Post
 
 
 class AppTest(unittest.TestCase):
     def test_menu_prints_prompt(self):
         with patch('builtins.input') as mocked_input:
             app.menu()
-            mocked_input.assert_called_once_with('Enter "c" to create a blog, "l" to list blogs, "r" to read one, "p" to create a post, or "q" to quit: ')
+            mocked_input.assert_called_with(app.MENU_PROMPT)
 
     def test_menu_calls_print_blogs(self):
         with patch('app.print_blogs') as mocked_print_blogs:
@@ -27,7 +28,7 @@ class AppTest(unittest.TestCase):
         app.blogs = {'Test': b}
         with patch('builtins.print') as mocked_print:
             app.print_blogs()
-            mocked_print.assert_called_with('- Test by Test Author (0 post)')
+            mocked_print.assert_called_with(f"- {b}")
 
     """
     def test_print_blogs_with_MagicMock(self):
@@ -37,3 +38,33 @@ class AppTest(unittest.TestCase):
         app.print_blogs()
         magic_method.assert_called_with('- Test by Test Author (0 post)')
     """
+
+    def test_ask_create_blog(self):
+        with patch('builtins.input') as mocked_input:
+            mocked_input.side_effect = ('Test', 'Test Author')
+            app.ask_create_blog()
+
+            self.assertIsNotNone(app.blogs.get('Test'))
+
+    def test_ask_read_blog(self):
+        b = Blog('Test', 'Test Author')
+        app.blogs = {'Test': b}
+        with patch('builtins.input', return_value='Test') as mocked_input:
+            with patch('app.print_posts') as mocked_print_posts:
+                app.ask_read_blog()
+                mocked_print_posts.assert_called_with(b)
+
+    def test_print_posts(self):
+        b = Blog('Test', 'Test Author')
+        b.create_post('Test Post', 'Test Post Content')
+        app.blogs = {'Test': b}
+        with patch('app.print_one_post') as mocked_print_post:
+            app.print_posts(b)
+            mocked_print_post.assert_called_with(b.posts[0])
+
+    def test_print_one_post(self):
+        p = Post('Test Post Title', 'Test Post Content')
+
+        with patch('builtins.print') as mocked_print:
+            app.print_one_post(p)
+            mocked_print.assert_called_with(f"--- Test Post Title --- \n\n Test Post Content \n\n")
